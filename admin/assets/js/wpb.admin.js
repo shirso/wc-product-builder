2e680d8c1074b774ec9fe63ce35b9872642387e2
SHIRSO <SHIRSO>
2016-02-18 00:15:44 +0530

    From Home

--- admin/assets/js/wpb.admin.js
+++ admin/assets/js/wpb.admin.js
@@ -11,4 +11,17 @@ jQuery(function($){
         });
         mediaUploader.open();
     });
+    var product_gallery_frame;
+    $(document).on('click','.wpb_multiple_image_upload',function(e){
+        e.preventDefault();
+        product_gallery_frame = wp.media.frames.downloadable_file = wp.media({
+            title: 'Manage Variation Images',
+            button: {
+                text: 'Add to variation',
+            },
+            multiple: true
+
+        });
+        product_gallery_frame.open();
+    });
 });
