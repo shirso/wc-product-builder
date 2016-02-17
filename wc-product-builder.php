9c6f41218a482fa283d4d3eca0fee6a0f9f65ea7
shirso <shirso@gmail.com>
2016-02-17 14:22:30 +0530

    Let's get started

---
+++ wc-product-builder.php
@@ -0,0 +1,19 @@
+<?php
+/*Plugin Name:Woocommerce Product Builder
+ Plugin URI: http://wp-theme.eu/de
+ Description: Create Multi-step Booking Module with WooCommerceVersion: 1.0.1
+ Author: WP-THEME.EU
+ Author URI:http://wp-theme.eu/de*/
+if (!defined('ABSPATH')) exit;
+if (!defined('WPB_PLUGIN_DIR')) define('WPB_PLUGIN_DIR', dirname(__FILE__));
+if (!defined('WPB_PLUGIN_ROOT_PHP')) define('WPB_PLUGIN_ROOT_PHP', dirname(__FILE__) . '/' . basename(__FILE__));
+if (!defined('WPB_PLUGIN_ABSOLUTE_PATH')) define('WPB_PLUGIN_ABSOLUTE_PATH', plugin_dir_url(__FILE__));
+if (!defined('WPB_PLUGIN_ADMIN_DIR')) define('WPB_PLUGIN_ADMIN_DIR', dirname(__FILE__) . '/admin');
+if (!class_exists('WPB_Booking_Module')) {
+    class WPB_Booking_Module{
+        public function __construct(){
+            require_once(WPB_PLUGIN_ADMIN_DIR . '/class-admin.php');
+        }
+    }
+    new WPB_Booking_Module();
+}
